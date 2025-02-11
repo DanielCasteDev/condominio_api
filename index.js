fa3ab9afa4b4c9f1939292bcb7a48de6fe16475c
Justin Daniel Castellanos Ortiz <justindanielcastellanosortiz@MacBook-Air-de-Justin.local>
2025-02-11 15:08:48 -0600

    pruebas4

--- index.js
+++ index.js
@@ -13,39 +13,8 @@ const app = express();
 // Conectar a MongoDB
 connectDB();
 
-// Función para normalizar el origen (eliminar la barra final)
-const normalizeOrigin = (origin) => {
-  return origin.endsWith("/") ? origin.slice(0, -1) : origin;
-};
-
-// Configuración de CORS para local y servidor
-const corsOptions = {
-  origin: (origin, callback) => {
-    const allowedOrigins = [
-      process.env.FRONTEND_URL_DEV,
-      process.env.FRONTEND_URL_PROD,
-    ];
-
-    // Normalizar los orígenes permitidos
-    const normalizedAllowedOrigins = allowedOrigins.map(normalizeOrigin);
-
-    // Normalizar el origen de la solicitud
-    const normalizedOrigin = normalizeOrigin(origin);
-
-    // Verificar si el origen está permitido
-    if (normalizedAllowedOrigins.includes(normalizedOrigin)) {
-      callback(null, true); // Permitir el origen
-    } else {
-      callback(new Error("Origen no permitido por CORS")); // Rechazar el origen
-    }
-  },
-  methods: "GET,POST,PUT,DELETE", // Métodos permitidos
-  allowedHeaders: "Content-Type,Authorization", // Cabeceras permitidas
-  credentials: true, // Permitir credenciales (cookies, tokens)
-};
-
 // Middleware
-app.use(cors(corsOptions)); // Habilitar CORS con opciones personalizadas
+app.use(cors()); // Habilitar CORS
 app.use(express.json());
 
 // Rutas sin autenticación (login no necesita verificar token)
